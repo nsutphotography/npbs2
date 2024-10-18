6099064a5bba37d75caa6ab9b5e54b9f60be46fa
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2024-10-18 20:55:33 +0530

    CORS origin set due to Cannot use wildcard in Access-Control-Allow-Origin when credentials flag is true.

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ connectDB();
 
 // Middleware to enable CORS
 app.use(cors(
-  // {
+  {
   // origin: 'http://localhost:5173', // Allow requests from this origin
-  // origin: process.env.FRONTEND_URL,  // Use the frontend URL from environment variable
-  // credentials: true // Allow credentials (cookies) to be sent
-// }
+  origin: process.env.FRONTEND_URL,  // Use the frontend URL from environment variable
+  credentials: true // Allow credentials (cookies) to be sent
+}
 ));
 // Middleware to parse JSON and urlencoded data
 app.use(express.json());
