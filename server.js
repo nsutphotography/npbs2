c24188508b1e6d196814eb3ab796b94ceaa43aba
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2024-10-19 00:54:59 +0530

    testing cors enable *

--- server.js
+++ server.js
@@ -22,13 +22,19 @@ const loginRoutes = require('./routes/login.routes');
 connectDB();
 
 // Middleware to enable CORS
-app.use(cors(
-  {
-  // origin: 'http://localhost:5173', // Allow requests from this origin
-  origin: process.env.FRONTEND_URL,  // Use the frontend URL from environment variable
-  credentials: true // Allow credentials (cookies) to be sent
-}
-));
+console.log('Frontend URL:', process.env.FRONTEND_URL);
+
+app.use(cors({
+  origin: '*', // Allow all origins temporarily for debugging
+  credentials: true // Allow credentials
+}));
+// app.use(cors(
+//   {
+//   // origin: 'http://localhost:5173', // Allow requests from this origin
+//   origin: process.env.FRONTEND_URL,  // Use the frontend URL from environment variable
+//   credentials: true // Allow credentials (cookies) to be sent
+// }
+// ));
 // Middleware to parse JSON and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
